3bff139d541b73dd77466891d9770ab28dd7a168
jjina8715 <junjina63@naver.com>
2019-05-29 23:11:37 +0900

    algorithm

---
+++ stack_queue/LinkedList.java
@@ -0,0 +1,67 @@
+package stack_queue;
+
+public class LinkedList {
+	LinkedNode first; // LinekedList의 첫 node
+	LinkedNode last; // LinekedList의 마지막 node
+
+	LinkedList() {
+		first = null;
+		last = null;
+	}
+
+	protected void addFirst(int val) {
+		LinkedNode newNode = new LinkedNode(val);
+		if(first == null) {
+			last = newNode;
+		}else {
+			newNode.next = first;
+		}
+		first = newNode;
+	}
+
+	protected void addLast(int val) {
+		LinkedNode newNode = new LinkedNode(val);
+		if(last == null) {
+			first = newNode;
+		}else {
+			last.next = newNode;
+		}
+		last = newNode;
+	}
+
+	protected Boolean removeFirst() {
+		if(first!=null) {
+			if(first!=last) {
+				first = first.next;
+			}else {
+				first=last=null;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	protected Boolean removeLast() {
+		if(last!=null) {
+			if(first==last) {
+				first=last=null;
+			}else {
+				LinkedNode cur = first;
+				while(cur.next != last) {
+					cur = cur.next;
+				}
+				last = cur;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	protected void printList() {
+		LinkedNode current = first;
+		while (current != null) { //current 노드가 null 아닐때 까지 실행
+			System.out.println(current.val);
+			current = current.next; //next 노드를 current에 덮어 씌움
+		}
+	}
+}
