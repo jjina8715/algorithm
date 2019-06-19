44a7700a5d4b5db8da343e2b66b89860e7925346
jjina8715 <junjina63@naver.com>
2019-06-20 00:39:37 +0900

    programmers

--- question/programmers/Tile.java
+++ question/programmers/Tile.java
@@ -1,22 +1,21 @@
-package programmers;
+package practice;
 
 public class Tile {
 	public static void main(String[] args) {
 		System.out.println(solution(5));
 	}
-	public static long solution(int n) {
-		long answer = 0;
-        answer = getRound(n)*2+(getRound(n-1)+getRound(n))*2;
-        return answer;
-    }
-    public static long getRound(long n){  	
+
+    public static long solution(int n) {
+    	long answer = 0;
+    	long[] sides = new long[n];
+    	sides[0] = 1;
+    	sides[1] = 1;
     	if(n==1)
-        	return 1;
-    	else if(n==2)
-    		return 1;
-    	else {
-    		
-    		return getRound(n-1)+getRound(n-2);
-    	}
+    		return 4;
+    	for(int i = 2; i<n;i++)
+    		sides[i] = sides[i-1]+sides[i-2];
+    	
+    	answer = sides[n-1]*2+(sides[n-2]+sides[n-1])*2;
+    	return answer;
     }
 }
