3440b710e50a409100d6ab1bde698be9bf21cc02
jjina8715 <junjina63@naver.com>
2019-06-29 21:38:38 +0900

    programmers

--- question/programmers/DP_back.java
+++ question/programmers/DP_back.java
@@ -1,31 +1,39 @@
-package programmers;
+package practice;
 
 import java.util.Stack;
 
 public class DP_back {
 
 	public static void main(String[] args) {
-		System.out.println(solution(4, 3, new int[][] {}));
+		System.out.println(solution(4,3, new int[][] {{2,2}}));
 	}
 	public static int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        long cnt = 0;
-        long[][] path = new long[n][m];
+		int answer = 0;
+        long[][] path = new long[m][n];
+        for(int[] p: puddles) {
+        	if(--p[0]==0)
+        		for(int i=p[1];i<n;i++)
+        			path[p[0]][i]=-1;
+        	if(--p[1]==0)
+        		for(int i=p[0];i<m;i++)
+        			path[i][p[1]]=-1;
+        		path[p[0]][p[1]] = -1;
+        }
         path[0][0]=1;
-        for(int[] p: puddles)
-        	path[p[1]][p[0]] = 0;
-        for(int i =0; i<n;i++) {
-        	for(int j=0; j<m;j++) {
-        		if(path[i][j]!=0) {
-        			if(i==0||i==n||j==0||j==m)
+        for(int i =0; i<m;i++) {
+        	for(int j=0; j<n;j++) {
+        		if(path[i][j]!=-1) {
+        			if(i==0||j==0) {
         				path[i][j] = 1;
-        			else{
-        				path[i][j] = path[i][j-1]+path[i-1][j];
+        			}else{
+        				path[i][j] = (path[i][j-1]+path[i-1][j])%1000000007L;
         			}
+        		}else {
+        			path[i][j]=0;
         		}
         	}
         }
-        answer = (int)(path[n-1][m-1]%1000000007L);
+        answer =  (int)path[m-1][n-1];
         /*Stack<int[]> s = new Stack<>();
 		
         for(int[] p : puddles)
