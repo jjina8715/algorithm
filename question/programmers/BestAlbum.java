f00dd0597d10028a7f09b061a2fa2c25f9419880
jjina8715 <junjina63@naver.com>
2019-07-04 17:54:22 +0900

    programmers

--- question/programmers/BestAlbum.java
+++ question/programmers/BestAlbum.java
@@ -1,10 +1,10 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -13,19 +13,57 @@ import java.util.Map.Entry;
 public class BestAlbum {
 
 	public static void main(String[] args) {
-		solution(new String[] {"classic", "pop", "classic", "classic", "pop"}, new int[] {500, 600, 150, 800, 2500});
+		
+		int[] ans = solution(
+				new String[] {"classic","pop","classic","classic","pop", "jazz", "jazz"},
+				new int[] {500, 600, 150, 800, 2500, 2000, 1000});
+		for(int i:ans)
+			System.out.print(i+" ");
 	}
 	public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
+        int sidx;
         Map<String, LinkedList<Integer[]>> list = new HashMap<String, LinkedList<Integer[]>>();
-        Map<String, Integer> g_plays = new LinkedHashMap<>();
+        Map<String, Integer> g_plays = new HashMap<>();
+        LinkedList<Integer[]> song = null;
+        ArrayList<Integer> ans = new ArrayList<>();
+        
         for(int i=0; i<genres.length;i++) {
-        	list.put(i, new String[] {genres[i], plays[i]+""});
-        	g_plays.put(genres[i], g_plays.getOrDefault(genres[i], plays[i])+plays[i]);
+        	song = list.getOrDefault(genres[i], new LinkedList<Integer[]>());
+        	song.add(new Integer[] {i,plays[i]});
+        	list.put(genres[i], song);
+        	g_plays.put(genres[i], g_plays.getOrDefault(genres[i], 0)+plays[i]);
         }
         Iterator<Map.Entry<String, Integer>> it = sortByPlays(g_plays).iterator();
-        while(it.hasNext())
-        	System.out.println(it.next().getValue());
+        while(it.hasNext()){
+        	song = list.get(it.next().getKey());
+        	Collections.sort(song, new Comparator<Integer[]>(){
+				@Override
+				public int compare(Integer[] o1, Integer[] o2) {
+					if(o1[1].equals(o2[1])) {
+						if(o1[0].compareTo(o2[0])==1)
+							return 1;
+						else
+							return -1;
+					}else if(o1[1]<o2[1])
+						return 1;
+					else 
+						return -1;
+				}
+        	});
+
+        	sidx=0;
+        	Iterator<Integer[]> it2 = song.iterator();
+        	while(it2.hasNext()&&sidx<2) {
+        		ans.add(it2.next()[0]);
+        		sidx++;
+        	}
+        }
+        int len = ans.size();
+        answer = new int[len];
+        for(int i=0;i<len;i++) {
+        	answer[i]=ans.get(i);
+        }
         return answer;
     }
 	public static List<Map.Entry<String, Integer>> sortByPlays(Map<String, Integer> map) {
