80c760da5054138d5fd3f13160c2f032b8e3af28
jina <junjina63@naver.com>
2019-08-09 17:43:44 +0900

    programmers

--- question/programmers/LargeNumber.java
+++ question/programmers/LargeNumber.java
@@ -3,10 +3,31 @@ package programmers;
 public class LargeNumber {
 
 	public static void main(String[] args) {
-		System.out.println("ans: " +solution("1924", 2));
+		System.out.println("ans: " + solution("1924", 2));
 	}
+
 	public static String solution(String number, int k) {
-        String answer = "";
-        return answer;
-    }
+		String answer = "";
+		int length = number.length(), len = length - k, idx = 0, str = 0, end = k + 1;
+		char[] ans = new char[len];
+		while (idx < len) {
+			ans[idx] = number.charAt(str);
+			if (end > length)
+				end = length;
+			for (int i = str + 1; i < end; i++) {
+				if (ans[idx] == 9)
+					break;
+				if (number.charAt(i) > ans[idx]) {
+					str = i;
+					ans[idx] = number.charAt(i);
+				}
+			}
+			idx++;
+			str++;
+			end++;
+		}
+
+		answer = String.copyValueOf(ans);
+		return answer;
+	}
 }
--- question/programmers/Lifeboat.java
+++ question/programmers/Lifeboat.java
@@ -6,24 +6,16 @@ import java.util.Arrays;
 public class Lifeboat {
 
 	public static void main(String[] args) {
-		System.out.println("ans: " + solution(new int[] { 50, 60, 70, 80, 90, 100, 100, 110, 120, 150, }, 200));
+		System.out.println("ans: " + solution(new int[] { 
+				50, 70, 50, 80 }, 100));
 	}
 
 	public static int solution(int[] people, int limit) {
-		int answer = 0, j;
-		boolean[] leave = new boolean[people.length];
+		int answer = 0, i, j;
 		Arrays.sort(people);
-		for (int i = 0; i < people.length; i++) {
-			if (!leave[i]) {
-				leave[i] = true;
-				for (j = people.length - 1; j > i; j--) {
-					if (!leave[j] && people[i] + people[j] <= limit) {
-						leave[j] = true;
-						break;
-					}
-				}
-				answer++;
-			}
+		for (i = 0, j=people.length-1;i<=j ; j--, answer++) {
+			if(people[i]+people[j]<=limit)
+				i++;
 		}
 		return answer;
 	}
