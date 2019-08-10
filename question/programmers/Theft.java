64adff7c418336bfe86437548c28f8ef725bbb3a
jjina8715 <junjina63@naver.com>
2019-08-10 23:36:40 +0900

    programmers

--- question/programmers/Theft.java
+++ question/programmers/Theft.java
@@ -1,35 +1,26 @@
-package programmers;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+package practice;
 
 public class Theft {
 	public static void main(String[] args) {
-		System.out.println("ans: " + solution(new int[] { 
-				1,2,3,1 }));
+		System.out.println("ans: " + solution(new int[] { 1, 2, 3, 1 }));
 	}
+
 	public static int solution(int[] money) {
-        int answer = 0;
-        
-        answer = money[0];
-        for(int i = 0; i<money.length-2;) {
-        	if(money[i+2]>money[i+3])
-        }
-        
-        return answer;
-    }
-	public static int solution2(int[] money) {
-        int answer = 0;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i<money.length;i++)
-        	map.put(i, money[i]);
-        
-        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
-		Collections.sort(list, (o1, o2)->{return o2.getValue().compareTo(o1.getValue());});
+		int answer = 0, len = money.length;
+		int[] stolen = new int[len];
 		
-        return answer;
-    }
+		stolen[0] = stolen[1] =  money[0];//첫번째 집을 훔쳤을 때
+		for (int i = 2; i < len - 1; i++) {
+			stolen[i] = Math.max(stolen[i-2]+money[i], stolen[i-2]);
+		}
+		answer = stolen[len-2];
+		stolen[0] = 0;
+		stolen[1] = money[1];//두번째 집을 훔쳤을 때
+		for (int i = 2; i < len; i++) {
+			stolen[i] = Math.max(stolen[i-2]+money[i], stolen[i-2]);
+		}
+		answer = Math.max(answer, stolen[len-1]);
+		return answer;
+	}
+
 }
