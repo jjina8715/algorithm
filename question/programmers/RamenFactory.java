241cc3dae6c1ff833013d1d9764ca2611fd223cd
jina <junjina63@naver.com>
2019-08-22 19:20:52 +0900

    programmers

---
+++ question/programmers/RamenFactory.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class RamenFactory {
+
+	public static void main(String[] args) {
+		System.out.println(solution(4,new int[] {4,10,15},new int[] {20,5,10},30));
+	}
+	static public int solution(int stock, int[] dates, int[] supplies, int k) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
