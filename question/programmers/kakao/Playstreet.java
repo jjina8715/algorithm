2af8f007e899b9ab93b72fe5457d52c3148ab00d
jina <junjina63@naver.com>
2019-09-04 16:34:56 +0900

    programmers

---
+++ question/programmers/kakao/Playstreet.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Playstreet {
+/*
+ * 3	3	{{0,0,0}, {0,0,0},{0,0,0}}	6
+3	6	[[0, 2, 0, 0, 0, 2], [0, 0, 2, 0, 1, 0], [1, 0, 0, 2, 2, 0]]	2
+ */
+	public static void main(String[] args) {
+		System.out.println(solution(3,3,new int[][] {{0,0,0}, {0,0,0},{0,0,0}}));
+	}
+	int MOD = 20170805;
+	public static int solution(int m, int n, int[][] cityMap) {
+		int answer = 0;
+		
+		return answer;
+	}
+}
